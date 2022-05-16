d35c2a5eddc99e669b734e4f50f9ff3c9ef391a8
Victoria <veejo90@gmail.com>
2022-05-15 22:18:16 -0400

    added db functions to animals and users

--- db/animals.js
+++ db/animals.js
@@ -1,36 +1,102 @@
 const client = require("./client");
 
-async function createAnimals
-({
-    id,
-    name,
-    biome, 
-    type,
-    description,
-    CareInstructions,
-    diet,
-    age,
-    CareDifficulty,
-    price
+async function createAnimals({
+  id,
+  name,
+  biome,
+  type,
+  description,
+  CareInstructions,
+  diet,
+  age,
+  CareDifficulty,
+  price,
 }) {
-
-    try {
-        const {rows: [animal]} = await client.query(`
+  try {
+    const {
+      rows: [animal],
+    } = await client.query(
+      `
         INSERT INTO animals(id, name, biome, type, description, "CareInstructions", diet, age, "CareDifficulty", price)
         VALUES($1,$2,$3,$4, $5, $6, $7, $8, $9, $10)
         RETURNING *;
         `,
-        [id, name, biome, type, description, CareInstructions, diet, age, CareDifficulty, price]);
-        return animal;
-   
-    }catch (error){
-        console.error(error);
-        throw error;
-    }
+      [
+        id,
+        name,
+        biome,
+        type,
+        description,
+        CareInstructions,
+        diet,
+        age,
+        CareDifficulty,
+        price,
+      ]
+    );
+    return animal;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 }
 
+async function getAllAnimals() {
+  try {
+    const { rows } = await client.query(`
+        SELECT *
+        FROM animals;
+        `);
+    console.log("These are all the animals", rows);
+    return rows;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
 
+async function getAnimalsById({ id }) {
+  try {
+    const {
+      rows: [animal],
+    } = await client.query(
+      `
+        SELECT *
+         FROM animals;
+         WHERE id=$1
+         `,
+      [id]
+    );
+    return animal;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+async function updateAnimal({ id, name, description }) {
+  try {
+    const {
+      rows: [animal],
+    } = await client.query(
+      `
+      UPDATE animals
+      SET name = $1, description =$2
+      WHERE id=$3
+      RETURNING *;
+    `,
+      [name, description, id]
+    );
+
+    return animal;
+  } catch (error) {
+    throw error;
+  }
+}
 
 module.exports = {
-    createAnimals,
-}
+  createAnimals,
+  getAllAnimals,
+  getAnimalsById,
+  updateAnimal,
+};
