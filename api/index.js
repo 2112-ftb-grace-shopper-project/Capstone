d3c0bd66095f921d5043b83a008c8ed589d80738
Qiang Huang <qhuan1223@gmail.com>
2022-05-16 21:14:39 -0400

    update api

--- api/index.js
+++ api/index.js
@@ -1,17 +1,62 @@
-const apiRouter = require('express').Router();
+// create an api router
+// attach other routers from files in this api directory (users, activities...)
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = process.env;
 
-apiRouter.get('/', (req, res, next) => {
+const express = require("express");
+const apiRouter = express.Router();
+
+const { getUserById } = require("../db");
+
+//health check
+apiRouter.get("/health", async (req, res, next) => {
   res.send({
-    message: 'API is under construction!',
+    message: "server is healthy",
   });
 });
 
-apiRouter.get('/health', (req, res, next) => {
-  res.send({
-    healthy: true,
-  });
+apiRouter.use(async (req, res, next) => {
+  const prefix = "Bearer ";
+  const auth = req.header("Authorization");
+
+  if (!auth) {
+    next();
+  } else if (auth.startsWith(prefix)) {
+    const token = auth.slice(prefix.length);
+    try {
+      const { id } = jwt.verify(token, JWT_SECRET);
+      if (id) {
+        req.user = await getUserById(id);
+        next();
+      }
+    } catch ({ name, message }) {
+      next({ name, message });
+    }
+  } else {
+    next({
+      name: "AuthorizationHeaderError",
+      message: `Authorization token must start with ${prefix}`,
+    });
+  }
 });
 
-// place your routers here
+const usersRouter = require("./users");
+apiRouter.use("/users", usersRouter);
+
+// const activitiesRouter = require("./activities");
+// apiRouter.use("/activities", activitiesRouter);
+
+// const routinesRouter = require("./routines");
+// apiRouter.use("/routines", routinesRouter);
+
+// const routineActivitiesRouter = require("./routine_activities");
+// apiRouter.use("/routine_activities", routineActivitiesRouter);
+
+apiRouter.use((error, req, res, next) => {
+  res.status(500).send({
+    name: error.name,
+    message: error.message,
+  });
+});
 
 module.exports = apiRouter;
---
+++ api/users.js
@@ -0,0 +1,105 @@
+// const { response } = require("express");
+const express = require("express");
+const usersRouter = express.Router();
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET = 'neverTell' } = process.env;
+const { requireUser } = require('./utils')
+
+const {
+  createUser,
+  getUserByUsername,
+  getUser,
+//   getPublicRoutinesByUser
+} = require("../db");
+// const { user } = require("pg/lib/defaults");
+
+
+usersRouter.post("/register", async (req, res, next) => {
+  const { username, password } = req.body;
+  try {
+    const _user = await getUserByUsername(username);
+    if (_user) {
+      next({
+        name: "userAlreadyExists",
+        message: "a user with that username already exists",
+      });
+      
+    } else if (password.length < 8) {
+      next({
+        name: "passwordTooShort",
+        message: "password must be at least 8 characters",
+      });
+    } else {
+      const user = await createUser({ username, password });
+      const token = jwt.sign(user, JWT_SECRET);
+      if(!user) {
+        next({
+          message: "error here no user"
+        })
+      }
+      res.send({user, token});
+    }
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
+
+
+
+usersRouter.post("/login", async (req, res, next) => {
+  const {username, password} = req.body;
+  
+  try {
+    if (!username|| !password) {
+      next({
+        name: "MissingCredentialsError",
+        message: "Please input both a username and password"
+      });
+    }
+    
+    const user = await getUser({username, password});
+    
+    if(!user) {
+      next({
+        name: "MissingCredentialError",
+        message: "There is no user. please sign up"
+      });
+    } 
+    
+    else {
+      const token = jwt.sign({
+        id: user.id,
+        username: user.username
+      }, JWT_SECRET);
+      
+      res.send({user, message: "thanks for signing up", token:token});
+    } 
+    
+  } catch ({name, message}) {
+    next({name, message});
+  }
+});
+
+
+usersRouter.get("/me", requireUser,  async(req, res, next) => {
+  try {
+    res.send(req.user)
+
+  } catch (error) {
+    next(error);
+  }
+})
+
+// usersRouter.get("/:username/routines", async(req, res, next) => {
+//   const { username } = req.params
+//   try {
+//     const routines = await getPublicRoutinesByUser({username});
+//     res.send(routines)
+//   } catch (error) {
+//     next(error)
+//   }
+// })
+
+
+
+module.exports = usersRouter;
