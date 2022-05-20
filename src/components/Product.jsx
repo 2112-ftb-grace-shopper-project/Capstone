1298b8149d8e820434186f8723d0ce876c14afbf
Jake Doan <jakedoan@Jakes-MacBook-Pro-2.local>
2022-05-19 21:28:10 -0500

    started product and productslist components

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
 
 const Product = () => {
+
+  const testAnimal =
+    {
+      name: "Zebra",
+      id: 100,
+      biome: "savannah",
+      type: "mammal",
+      description: "neigh",
+      careinstructions: "enter in kentucky derby",
+      diet: "herbivore",
+      age: 4,
+      difficulty: "easy",
+      price: 4000,
+      image: `document.getElementById('id01gorillaphant').src="assets/ExoticAnimals/id01gorillaphant.jpg"`
+    }
+
+
   return (
-    <div>Product</div>
+    <div>
+      <h1>{testAnimal.name}</h1>
+      <p>{testAnimal.image}</p>
+      <p>{testAnimal.price}</p>
+      <p>{testAnimal.biome}</p>
+      <p>{testAnimal.type}</p>
+      <p>{testAnimal.description}</p>
+      <p>{testAnimal.careinstructions}</p>
+      <p>{testAnimal.diet}</p>
+      <p>{testAnimal.age}</p>
+      <p>{testAnimal.difficulty}</p>
+      {/* Maybe add below to new component? maybe look at MUI components? */}
+      <form>
+        <label>Quantity: </label>
+        <input type="text" name="Quantity" /> 
+      </form>
+      <button>Add To Cart</button>
+    </div>
 
     //Display individual product info (pic, description, care, age, price, etc)
     //Input field for quantity to buy
