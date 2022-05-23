b6512ed127e90b18f2d544e5aadb0a0b39ba1b7f
Colin Hoolwerf <choolwerf@gmail.com>
2022-05-23 16:14:16 -0500

    making sure the default route is working

--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -2,65 +2,79 @@ import React from 'react'
 
 const ProductsList = () => {
 
-  const testAnimals =[
+  const testAnimals = [
     {
-      name: "Zebra",
-      id: 100,
-      biome: "savannah",
-      type: "mammal",
-      description: "neigh",
-      careinstructions: "enter in kentucky derby",
-      diet: "herbivore",
-      age: 4,
-      difficulty: "easy",
-      price: 4000,
-      image: `document.getElementById('id01gorillaphant').src="assets/ExoticAnimals/id01gorillaphant.jpg"`
+      "name" : "Gorillaphant",
+      "id" : 1,
+      "biome" : "Savannah",
+      "type" : "Mammal",
+      "description" : "Half Gorilla, Half Elephant, same full price!",
+      "CareInstructions" : "Do NOT challenge to fight. Do NOT make eye contact. Do NOT try to take its stick.",
+      "diet" : "Omnivore",
+      "age" : 8,
+      "CareDifficulty" : "Hard",
+      "price" : "$5000",
+      "image" : "../assets/ExoticAnimals/id01gorillaphant"
     },
     {
-      name: "Ostrich",
-      id: 101,
-      biome: "savannah",
-      type: "bird",
-      description: "angry as hell",
-      careinstructions: "definitely enter in kentucky derby instead of zebra",
-      diet: "carnivore",
-      age: 5,
-      difficulty: "hard",
-      price: 2500,
-      image: ""
+      "name" : "Orangutans",
+      "id" : 2,
+      "biome" : "Forest",
+      "type" : "Mammal",
+      "description" : "Loveable orange boys, very fond of climbing trees and eating fruit. *Sunglasses sold separately*",
+      "CareInstructions" : "A normal sized tree house with a tire swing should do the trick for space. Be sure to stay stocked up on bananas.",
+      "diet" : "Omnivore",
+      "age" : 10,
+      "CareDifficulty" : "medium", 
+      "price" : "$1000",
+      "image" : "../assets/ExoticAnimals/id02orangutans"
     },
     {
-      name: "Orca",
-      id: 102,
-      biome: "ocean",
-      type: "mammal",
-      description: "probably the most ferocious predators in the ocean",
-      careinstructions: "don't be a sea lion taking a nap",
-      diet: "carnivore",
-      age: 6,
-      difficulty: "hard",
-      price: 10000,
-      image: ""
+      "name" : "Zebrafrog",
+      "id" : 3,
+      "biome" : "Forest",
+      "type" : "Other",
+      "description" : "This handsome guy can hang from any surface, and camouflages perfectly with a herd of zebras.",
+      "CareInstructions" : "Prefers a dark, moist terrarium environment. Strongly prefers insects, but hay or savannah grass will work in a pinch.",
+      "diet" : "carnivore",
+      "age" : 4,
+      "CareDifficulty" : "easy",
+      "price" : "$50",
+      "image" : "../assets/ExoticAnimals/id03zebrafrog"
     },
     {
-      name: "Salamander",
-      id: 103,
-      biome: "pond",
-      type: "amphibian",
-      description: "slimey yet satisffying",
-      careinstructions: "don't be a sea lion taking a nap",
-      diet: "carnivore",
-      age: 2,
-      difficulty: "easy",
-      price: 600,
-      image: ""
+      "name" : "Red Panda",
+      "id" : 4,
+      "biome" : "Forest",
+      "type" : "Mammal",
+      "description" : "cute little face and red coloring, bushy tail ",
+      "CareInstructions" : "distant lover; feed him and give him space.",
+      "diet" : "vegetarian carnivore; mostly bamboo but feel free to throw in a bug or two",
+      "age" : "3",
+      "CareDifficulty" : "easy",
+      "price" : "$600",
+      "image" : "../assets/ExoticAnimals/id04redpanda"
+    },
+    {
+      "name" : "Stitch",
+      "id" : 5,
+      "biome" : "Hawaii",
+      "type" : "Mammal",
+      "description" : "An Originally illegally-made extraterrestrial life-form. Fond of high powered lasers,space-ships and surfboards. Indestructible,intelligent and abnoramally strong ",
+      "CareInstructions" :"good luck",
+      "diet" :"Loves SPAM",
+      "age" : 626,
+      "CareDifficulty" :"hard",
+      "price" : "$2",
+      "image" : "../assets/ExoticAnimals/id05stitch"
     },
   ]
+
   return (
     <div>
       {testAnimals.map((animal) => (
         <div key={animal.id}>
-          <p>{animal.image}</p>
+          {/* <p>{animal.image}</p> */}
           <h3>{animal.name}</h3>
           <p>{animal.biome}</p>
           <p>{animal.type}</p>
