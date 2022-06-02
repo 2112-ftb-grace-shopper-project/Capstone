3b3ef8c9caf8b3d600e1c71927cc673d2f2df78c
Jake Doan <95443495+jakedoan3@users.noreply.github.com>
2022-06-01 19:49:36 -0500

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
 import { 
   Navbar,
   Login,
@@ -7,15 +7,19 @@ import {
   Register,
   ProductsList,
   Product,
-  Cart
- } from '.'
+  Cart,
+  MyOrders
+ } from './'
 import '../style/App.css';
-import { getAnimals, getNewOrder, getSingleAnimal } from '../axios-services';
+import StripeCheckout from 'react-stripe-checkout';
+import { getAnimals } from '../axios-services';
 
 const App = () => {
   const [loggedIn, setLoggedIn] = useState(false);
   const [animalList, setAnimalList] = useState([]);
-  const [order, setOrder] = useState({})
+  const [myOrderList, setMyOrderList] = useState([]);
+  // const [cart, setCart] = useState([])
+  // const [order, setOrder] = useState({})
   
   useEffect(() => {
     const fetchAnimals = () => {
@@ -29,6 +33,20 @@ const App = () => {
     localStorage.setItem("cart", [])
   }, []);
 
+  // useEffect(() => 
+  // {
+  //   const fetchUser = () => {
+  //     let localUser = localStorage.getItem("username")
+  //     getSingleUser(localUser)
+  //     .then((result) => {
+  //       console.log("new useEffect" + result)
+
+  //     })
+  //     .catch(console.error)
+  //   }
+  //   fetchUser()
+
+  // })
   // useEffect(() => {
   //   const createOrder = () => {
   //     if (!order) {
@@ -42,6 +60,7 @@ const App = () => {
   //   const animal = await getSingleAnimal(animal)
   //   const animalFind = animal.find(animal => animal.id === +animalId)
   //   return animalFind;
+
   // }, [setSingleAnimal(animalFind)])
 
   useEffect (() => {
@@ -52,13 +71,14 @@ const App = () => {
   }, [])
 
   return (
-    <div>
+    <div className="app-container">
       <Navbar loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
       <Routes>
         <Route path="/Login" element={<Login setLoggedIn={setLoggedIn} />} />
         <Route path="/Register" element={<Register setLoggedIn={setLoggedIn} />} />
         <Route path="/Logout" element={<Logout />} />
         <Route path="/Cart" element={<Cart loggedIn={loggedIn} animalList={animalList}/>} />
+        <Route path="myorders" element={<MyOrders myOrderList={myOrderList} setMyOrderList={setMyOrderList}/>} loggedIn={loggedIn} />
         {/* <Route path="/ProductsList" element={<ProductsList />} /> */}
         <Route path="/animals/:animalId" element={<Product />} />
         <Route path="/" element={<ProductsList animalList={animalList} />} />
