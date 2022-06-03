3891821ea6e800ba5d9d8189f416c00472ab620f
ColinH58 <choolwerf@gmail.com>
2022-06-03 15:48:00 -0500

    moved sort into it's own component

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -54,29 +54,20 @@ const Product = ({cart, setCart}) => {
       />
       <div className="SingleAnimalInfo">
       <h3>{singleAnimal.name}</h3>
-      <p>Biome Type <hr /> {singleAnimal.biome}</p>
-      <br />
-      <p>Animal Type <hr /> {singleAnimal.type}</p>
-      <br />
-      <p>Description <hr /> {singleAnimal.description}</p>
-      <br />
-      <p>How to Handle <hr /> {singleAnimal.CareInstructions}</p>
-      <br />
-      <p>Age <hr /> {singleAnimal.age}</p>
-      <br />
-      <h5>Dietary Restrictions <hr /> {singleAnimal.diet}</h5>
-      <br />
-      <h5>Care Difficulty <hr /> {singleAnimal.CareDifficulty}</h5>
-      <br />
+      <p><strong>Biome Type</strong> <br /> {singleAnimal.biome}</p>
+      <p><strong>Animal Type</strong> <br /> {singleAnimal.type}</p>
+      <p><strong>Description</strong> <br /> {singleAnimal.description}</p>
+      <p><strong>How to Handle</strong> <br /> {singleAnimal.CareInstructions}</p>
+      <p><strong>Age</strong> <br /> {singleAnimal.age}</p>
+      <p><strong>Dietary Restrictions</strong> <br /> {singleAnimal.diet}</p>
+      <p><strong>Care Difficulty</strong> <br /> {singleAnimal.CareDifficulty}</p>
       <h3>TOTAL: {singleAnimal.price} (+tax, shipping, and handling)</h3>
       <br />
-      <hr></hr>
-      <p>How Many Would You Like to Add</p>
+      <p>Quantity</p>
       <button onClick={handleIncrease}>+</button>
       <p>{animalQuantity}</p>
       <button onClick={handleDecrease}>-</button>
-      <br />
-      <hr></hr>
+      <hr />
       <button onClick={addToCart}>Add to Cart</button>
       </div>
 
